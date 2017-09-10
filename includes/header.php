4d3b2d5ebd4344dfcf443dfebf2396e06307535e
root <christian.hernandez@yahoo.com>
2017-09-10 15:05:34 -0700

    This is why the cat shouldn't sit on my keyboard.

--- includes/header.php
+++ includes/header.php
@@ -3,6 +3,8 @@ require_once("config/config.php");
 //
 if (isset($_SESSION['username'])) {
 	$user_logged_in = $_SESSION['username'];
+	$user_details_query = mysqli_query($con, "SELECT * FROM users WHERE user_name = '{$user_logged_in}' ");
+	$user = mysqli_fetch_array($user_details_query);
 } else {
 	header("Location: register.php");
 }
@@ -10,5 +12,26 @@ if (isset($_SESSION['username'])) {
 <html>
 	<head>
 	<title>Welcome to Bumper</title>
+	<!-- Javascript -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+	<script src="assets/js/bootstrap.js"></script>
+
+	<!-- Sytlesheets -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css"></link>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css"></link>
 	</head>
 <body>
+	<div class="top_bar">
+		<div class="logo">
+			<a href="index.php">Bumper</a>
+		</div>
+		<nav>
+			<a href="#"><?php echo $user['first_name'] ?></a>
+			<a href="index.php"><i class="fa fa-home fa-lg"></i></a>
+			<a href="#"><i class="fa fa-envelope fa-lg"></i></a>
+			<a href="#"><i class="fa fa-bell-o fa-lg"></i></a>
+			<a href="#"><i class="fa fa-users fa-lg"></i></a>
+			<a href="#"><i class="fa fa-cog fa-lg"></i></a>
+		</nav>
+	</div>
--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php require_once("includes/header.php");?>
-		<h1>Welcome to Bumper!</a></h1>
+		<!-- site Content -->
 <?php require_once("includes/footer.php");?>

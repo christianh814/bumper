ee62284153e8a8cc5a274bd280597e3160b257ae
root <christian.hernandez@yahoo.com>
2017-09-21 07:27:40 -0700

    I must sleep... it's working... in just three hours...

--- profile.php
+++ profile.php
@@ -87,8 +87,8 @@ if (isset($_POST['post_message'])) {
 
 <div class="profile_main_column column">
 	<ul class="nav nav-tabs" role="tablist" id="profileTabs">
-  		<li role="presentation" class="active"><a href="#newsfeed_div" aria-controls="newsfeed_div" role="tab" data-togle="tab">Newsfeed</a></li>
-  		<li role="presentation"><a href="#messages_div" aria-controls="messages_div" role="tab" data-togle="tab">Messages</a></li>
+  		<li role="presentation" class="active"><a href="#newsfeed_div" aria-controls="newsfeed_div" role="tab" data-toggle="tab">Newsfeed</a></li>
+  		<li role="presentation"><a href="#messages_div" aria-controls="messages_div" role="tab" data-toggle="tab">Messages</a></li>
 	</ul>
 	<div class="tab-content">
 		<div role="tabpanel" class="tab-pane fade in active" id="newsfeed_div">
